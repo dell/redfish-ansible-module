bf4e1fc16ae62b913b1877a07cbda42120298a63
Pujeri, Prabhakar <Prabhakar_Pujeri@Dell.com>
2017-08-16 18:13:30 +0530

    included all sysinfo calls

--- library/idrac.py
+++ library/idrac.py
@@ -61,6 +61,7 @@ ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
                     'version': '0.1'}
 import requests
+import os
 from ansible.module_utils.basic import AnsibleModule
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
@@ -86,19 +87,104 @@ class iDRAC(object):
     
     def get_system_health(self):
         resp = self.send_get_request(self.system_uri)
-        return resp[u'Status'][u'Health']
+        return str(resp[u'Status'][u'Health'])
     
     def get_system_serial_number(self):
         resp = self.send_get_request(self.system_uri)
-        return resp[u'SerialNumber']
+        return str(resp[u'SerialNumber'])
     
     def get_system_service_tag(self):
         resp = self.send_get_request(self.system_uri)
-        return resp[u'SKU']
+        return str(resp[u'SKU'])
     
     def get_server_part_number(self):
         resp = self.send_get_request(self.system_uri)
-        return resp[u'PartNumber']
+        return str(resp[u'PartNumber'])
+    
+    def get_system_Manufacturer(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'Manufacturer'])
+    
+    def get_system_bios_version(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'BiosVersion'])
+    
+    def get_system_type(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'SystemType'])
+    
+    def get_system_power_state(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'PowerState'])
+    
+    def get_system_memory_health(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'MemorySummary'][u'Status'][u'Health'])
+    
+    def get_system_memory_in_GB(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'MemorySummary'][u'TotalSystemMemoryGiB'])
+    
+    def get_processor_count(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'ProcessorSummary'][u'Count'])
+    
+    def get_processor_health(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'ProcessorSummary'][u'Status'][u'Health'])
+    
+    def get_processor_model(self):
+        resp = self.send_get_request(self.system_uri)
+        return str(resp[u'ProcessorSummary'][u'Model'])
+    def get_boot_sources(self):
+        sources=[]
+        resp = self.send_get_request(self.system_uri+'/BootSources')
+        if 'UefiBootSeq' in resp[u'Attributes']:
+                for i in resp[u'Attributes']['UefiBootSeq']:
+                        sources.append(i['Name'])
+        return " ".join(str(x) for x in sources)
+        
+    def get_system_ethernet_interfaces(self):
+        eth=[]
+        resp = self.send_get_request(self.system_uri+'/EthernetInterfaces')
+        for i in resp[u'Members']:
+            eth.append(os.path.basename(i['@odata.id']))
+        return " ".join(str(x) for x in eth)
+
+    def get_system_ethernet_permanent_MAC_address(self):
+        resp = self.send_get_request(self.system_uri+'/EthernetInterfaces/%s'%self.module.params['eth_interface'])
+        return resp[u'PermanentMACAddress']
+    def get_system_secure_boot_status(self):
+        resp = self.send_get_request(self.system_uri+'/SecureBoot')
+        return resp[u'SecureBootCurrentBoot']
+    def get_system_secure_boot_certificates(self):
+        cert=[]
+        resp = self.send_get_request(self.system_uri+'/SecureBoot/Certificates')
+        for i in resp[u'Members']:
+            cert.append(os.path.basename(i['@odata.id']))
+        return " ".join(str(x) for x in cert)
+
+    def get_system_storage_controllers(self):
+        ctrls=[]
+        resp = self.send_get_request(self.system_uri+'/Storage/Controllers')
+        for i in resp[u'Members']:
+            ctrls.append(os.path.basename(i['@odata.id']))
+        return " ".join(str(x) for x in ctrls)
+
+    def get_system_storage_controller_disks(self):
+        disk=[]
+        resp = self.send_get_request(self.system_uri+'/Storage/Controllers/%s'%self.module.params['controller'])
+        return resp[u'Devices']
+    
+    def get_firmware_inventory(self):
+        fw=dict()
+        resp = self.send_get_request(self.updatesvc_uri+'/FirmwareInventory')
+        for i in resp[u'Members']:
+            fw_info=self.send_get_request(self.updatesvc_uri+'/FirmwareInventory/'+ '%s'%os.path.basename(i['@odata.id']))
+            fw[fw_info['Name']]=fw_info['Version']
+        return " ".join(str(x) for x in fw)
+        
+
     
 def main():
     # Parsing argument file
@@ -144,6 +230,57 @@ def main():
             
         if params['cmd'] == 'AssetTag':
             out=idrac.get_boot_sources()
+            
+        if params['cmd'] == 'Manufacturer':
+            out=idrac.get_system_Manufacturer()
+            
+        if params['cmd'] == 'BiosVersion':
+            out=idrac.get_system_bios_version()
+            
+        if params['cmd'] == 'SystemType':
+            out=idrac.get_system_type()
+            
+        if params['cmd'] == 'PowerState':
+            out=idrac.get_system_power_state()
+            
+        if params['cmd'] == 'MemoryHealth':
+            out=idrac.get_system_memory_health()
+            
+        if params['cmd'] == 'TotalSystemMemoryGiB':
+            out=idrac.get_system_memory_in_GB()
+            
+        if params['cmd'] == 'ProcessorCount':
+            out=idrac.get_processor_count()
+            
+        if params['cmd'] == 'ProcessorHealth':
+            out=idrac.get_processor_health()
+            
+        if params['cmd'] == 'ProcessorModel':
+            out=idrac.get_processor_model()
+            
+        if params['cmd'] == 'BootSources':
+            out=idrac.get_boot_sources()
+            
+        if params['cmd'] == 'EthernetInterfaces':
+            out=idrac.get_system_ethernet_interfaces()
+            
+        if params['cmd'] == 'PermanentMACAddress':
+            out=idrac.get_system_ethernet_permanent_MAC_address()
+            
+        if params['cmd'] == 'SecureBoot':
+            out=idrac.get_system_secure_boot_status()
+            
+        if params['cmd'] == 'SecureBootCerts':
+            out=idrac.get_system_secure_boot_certificates()
+            
+        if params['cmd'] == 'StorageControllers':
+            out=idrac.get_system_storage_controllers()
+            
+        if params['cmd'] == 'StorageControllerDisks':
+            out=idrac.get_system_storage_controller_disks()
+            
+        if params['cmd'] == 'FirmwareInventory':
+            out=idrac.get_firmware_inventory()
             
     if rc is None:
         result['changed']=False
