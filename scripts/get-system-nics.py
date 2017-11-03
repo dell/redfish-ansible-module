69a534caa8b80f44604e45e2bdd34080049d2725
Jose Delarosa <jose.delarosa@dell.com>
2017-11-03 15:03:19 -0500

    Added script to extract NIC info + minor update to get-system-disks script.

--- scripts/get-system-nics.py
+++ scripts/get-system-nics.py
@@ -16,9 +16,9 @@ import json
 import sys
 rf = rfutils.rfutils()
 
-def get_list_of_controllers(idrac, base_uri, rf_uri):
+def get_list_of_interfaces(idrac, base_uri, rf_uri):
     count = 0
-    controllers = []
+    interfaces = []
     response = rf.send_get_request(idrac, base_uri + rf_uri)
     rf.print_bold("status_code: %s" % response.status_code)
     if not response.status_code == 200:
@@ -26,47 +26,48 @@ def get_list_of_controllers(idrac, base_uri, rf_uri):
         exit(1)
     data = response.json()
 
-    for controller in data[u'Members']:
-        c = controller[u'@odata.id']
-        # Only PERC? What about lower-end systems with SATA controllers only?
-        # if "RAID" in c or "PERC" in c:
-        controllers.append(c)
-    return controllers
+    for interface in data[u'Members']:
+        c = interface[u'@odata.id']
+        interfaces.append(c)
+    return interfaces
 
-def get_controller_disks(idrac, base_uri, controllers):
-    disks = []
-    for c in controllers:
-        uri = base_uri + c
+def get_interface_details(idrac, base_uri, interfaces):
+    for i in interfaces:
+        uri = base_uri + i
         response = rf.send_get_request(idrac, uri)
         if not response.status_code == 200:
             rf.print_red("Something went wrong.")
             exit(1)
         data = response.json()
-        rf.print_bold("Controller name: %s" % data[u'Name'])
-
-        for disk in data[u'Devices']:
-            rf.print_bold("  Name: %s" % disk[u'Name'])
-            print("   Mfg: %s" % disk[u'Manufacturer'])
-            print("   Model: %s" % disk[u'Model'])
-            print("   State: %s" % disk[u'Status'][u'State'])
-            print("   Health: %s\n" % disk[u'Status'][u'Health'])
-            disks.append(disk)
-    return disks
+        rf.print_bold("Name: %s" % data[u'Name'])
+        print(" FQDN: %s" % data[u'FQDN'])
+        for info in data[u'IPv4Addresses']:
+            print(" IPv4: %s" % info[u'Address'])
+            print(" Gateway: %s" % info[u'GateWay'])
+            print(" SubnetMask: %s" % info[u'SubnetMask'])
+        for info in data[u'IPv6Addresses']:
+            print(" IPv6: %s" % info[u'Address'])
+        for info in data[u'NameServers']:
+            print(" NameServer: %s" % info)
+        print(" MAC: %s" % data[u'PermanentMACAddress'])
+        print(" Speed: %s Mbps" % data[u'SpeedMbps'])
+        print(" MTU: %s" % data[u'MTUSize'])
+        print(" Autoneg: %s" % data[u'AutoNeg'])
+        print(" State: %s" % data[u'Status'][u'State'])
+        print(" Health: %s\n" % data[u'Status'][u'Health'])
+    return
 
 def main():
     # Initialize iDRAC arguments
     idrac = rf.check_args(sys)
     base_uri = "https://" + idrac['ip']
-    rf_uri = "/redfish/v1/Systems/System.Embedded.1/Storage/Controllers/"
+    rf_uri = "/redfish/v1/Systems/System.Embedded.1/EthernetInterfaces/"
 
-    # Get all devices
-    controllers = get_list_of_controllers(idrac, base_uri, rf_uri)
+    # Get all interfaces
+    interfaces = get_list_of_interfaces(idrac, base_uri, rf_uri)
 
     # Go through list of devices and get detailed information for each one
-    disks = get_controller_disks(idrac, base_uri, controllers)
-
-    # Uncomment if you want to see all fields returned
-    # for d in disks: print(d)
+    get_interface_details(idrac, base_uri, interfaces)
 
 if __name__ == '__main__':
     main()
